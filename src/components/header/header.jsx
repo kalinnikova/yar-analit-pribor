516380a176c0229055060e958a269cf3aa56d532
a.kalinnikova <a.kalinnikova@accenture.com>
2019-05-24 11:14:53 +0300

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export class Header extends React.PureComponent {
+    render() {
+        return (
+            <div>
+                <h1>
+                    ЯрАналитПрибор
+                </h1>
+            </div>
+        )
+    }
+}

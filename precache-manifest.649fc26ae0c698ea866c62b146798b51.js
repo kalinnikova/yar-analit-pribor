0b72ef03ca37997bddd9e7f7c95bfa9b634df1c0
Aleksa <50983463+kalinnikova@users.noreply.github.com>
2019-07-18 17:25:58 +0300

    Add files via upload

---
+++ precache-manifest.649fc26ae0c698ea866c62b146798b51.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7c25d474d0bf083096f3b6d42b80ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5495a92723f3faf9d67",
+    "url": "/static/css/2.d8362722.chunk.css"
+  },
+  {
+    "revision": "00d04094750478d434d5",
+    "url": "/static/css/main.271ae76c.chunk.css"
+  },
+  {
+    "revision": "e5495a92723f3faf9d67",
+    "url": "/static/js/2.1502c2a9.chunk.js"
+  },
+  {
+    "revision": "00d04094750478d434d5",
+    "url": "/static/js/main.befede13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2098bce721d2052d6afd915ec937d851",
+    "url": "/static/media/YAMZ.2098bce7.png"
+  },
+  {
+    "revision": "e11491aabfc852e9e2f44f44d937ba98",
+    "url": "/static/media/YASHZ.e11491aa.png"
+  },
+  {
+    "revision": "6ae5a8fa374e2edc48d8ca1671b9f929",
+    "url": "/static/media/contacts.6ae5a8fa.jpg"
+  },
+  {
+    "revision": "585cfdac9b3a77d21d56606261f192fb",
+    "url": "/static/media/gazovik.585cfdac.jpg"
+  },
+  {
+    "revision": "4c2aa7700c285968a4c117303ce81901",
+    "url": "/static/media/logo.4c2aa770.png"
+  },
+  {
+    "revision": "f6cfee1812ae772f4aa20e2e282c0055",
+    "url": "/static/media/nkz.f6cfee18.jpg"
+  },
+  {
+    "revision": "5f905f34dd806281b49abb3d6da46f08",
+    "url": "/static/media/rzhd.5f905f34.png"
+  },
+  {
+    "revision": "fc0741bde9753397754e345cc013fd24",
+    "url": "/static/media/seti1.fc0741bd.png"
+  },
+  {
+    "revision": "79266cfe684956afdec0b48be6553b4a",
+    "url": "/static/media/seti2.79266cfe.png"
+  },
+  {
+    "revision": "9c72b55d9d34661b4b8acc1965808362",
+    "url": "/static/media/sro1.9c72b55d.jpg"
+  },
+  {
+    "revision": "b116b3ef87d67f57805368a64c1feda9",
+    "url": "/static/media/sro2.b116b3ef.jpg"
+  },
+  {
+    "revision": "4d4d3794586068c2035f7067e9ba3aca",
+    "url": "/static/media/tgk2.4d4d3794.png"
+  },
+  {
+    "revision": "712d6e15efd1f69c9f7586c3b284a713",
+    "url": "/static/media/vzlet5.712d6e15.png"
+  },
+  {
+    "revision": "88abcc983a617c7545cf1ddc99c10a64",
+    "url": "/static/media/zhb.88abcc98.png"
+  }
+]);
